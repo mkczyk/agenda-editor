5ff0f362c7d4c65cb8e8c674536a50ef578aceab
Dzakus <Dzakus13@gmail.com>
2016-05-13 18:11:55 +0200

    Add drag and drop

--- home/home.js
+++ home/home.js
@@ -8,9 +8,15 @@ angular.module('agendaEditor')
 			});
 	})
 
-	.controller('homeController', function($scope, Agenda) {
+	.controller('homeController', function($scope, Agenda, dragulaService) {
 		$scope.agenda = Agenda;
-		
+
+		dragulaService.options($scope, 'items-bag', {
+			moves: function (el, container, handle) {
+				return handle.classList.contains('handle');
+			}
+		});
+
 		$scope.addEmptyItem = function(){
 			Agenda.list.push({title: "", name: "", surname: ""});
 		};
